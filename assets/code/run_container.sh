64d1cffbbc999219d7c40ee6bca4188e0e006446
Andrew Pruski <andrew.pruski@channeladvisor.com>
2020-11-02 16:33:00 +0000

    updated slides

--- assets/code/run_container.sh
+++ assets/code/run_container.sh
@@ -3,13 +3,13 @@ docker run -d
 --env SA_PASSWORD=Testing1122
 --env ACCEPT_EULA=Y
 --env MSSQL_AGENT_ENABLED=True
---env MSSQL_DATA_DIR=/var/opt/sqlserver/data
---env MSSQL_LOG_DIR=/var/opt/sqlserver/log
---env MSSQL_BACKUP_DIR=/var/opt/sqlserver/backup
+--env MSSQL_DATA_DIR=/var/opt/sqlserver/sqldata
+--env MSSQL_LOG_DIR=/var/opt/sqlserver/sqllog
+--env MSSQL_BACKUP_DIR=/var/opt/sqlserver/sqlbackups
 --network sqlserver
 --volume sqlsystem:/var/opt/mssql
---volume sqldata:/var/opt/sqlserver/data
---volume sqllog:/var/opt/sqlserver/log
---volume sqlbackup:/var/opt/sqlserver/backup
---name testcontainer1
+--volume sqldata:/var/opt/sqlserver/sqldata
+--volume sqllog:/var/opt/sqlserver/sqllog
+--volume sqlbackup:/var/opt/sqlserver/sqlbackups
+--name sqlcontainer1
 mcr.microsoft.com/mssql/server:2019-CU5-ubuntu-18.04
